c99712c20faae3ff65e8ef2e5cb10d4b6de1891f
CcodeN206 <ns770947@gmail.com>
2025-08-31 17:08:57 +0000

    First/python/Datatype/type2.py

---
+++ First/python/Datatype/type2.py
@@ -0,0 +1,18 @@
+# Cheking datatype of variable
+
+a=10
+print(type(a))  # print datatype of a
+
+b=10.5
+print(type(b)) # print datatype of b
+b=20.5
+print(type(b)) # print datatype of b
+
+c=True
+print(type(c)) # print datatype of c
+
+d="hello"
+print(type(d)) # print datatype of d    
+
+e=3+5j              
+print(type(e)) # print datatype of e

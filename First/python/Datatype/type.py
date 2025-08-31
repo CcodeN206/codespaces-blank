91dc1aa79ee0e7c036e72e87fcc735c40a95f2fd
CcodeN206 <ns770947@gmail.com>
2025-08-31 22:33:58 +0530

    Update type.py

--- First/python/Datatype/type.py
+++ First/python/Datatype/type.py
@@ -1,19 +1,21 @@
 #we don't need to import any library for this to print data type
 #type of quotes
+# for string type data
 print("Hello this is double quote") # print string
 print ('This is single quote string') # print string
 print("""This is double triple quote string""") # print string
 print('''This is also  single triple quote string''') # print string
 
 # for number
-print(0.3) # float 
-print(1)  # int
-print(89) #double
+#we don't need to specify type of data while printing number whether it is float or int 
+print(0.3) # float type
+print(1)  # int type
+print(89) #double type
 
 
-#for boolean
+#for boolean type data 
 print(True) # print true
 print(False) # print false 
 
-#for complex
+#for complex 
 print(3+5j) # print complex number
